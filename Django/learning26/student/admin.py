a102a66d7f8906cc173ccebf53694598b8a5c356
Kashish Jansari <jansarikashish@gmail.com>
2026-02-04 12:49:59 +0530

    04-02-26:In the student app add table or make a class in model.py file and register it into the admin Django ORM

--- Django/learning26/student/admin.py
+++ Django/learning26/student/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import student,product,car
+ 
 # Register your models here.
+
+admin.site.register(student)
+admin.site.register(product)
+admin.site.register(car)
--- Django/learning26/student/models.py
+++ Django/learning26/student/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+class student(models.Model):
+    studentName=models.CharField(max_length=50)
+    studentAge=models.IntegerField()
+    studentCity=models.CharField(max_length=50)
+    studentEmail=models.EmailField(null=True)
+
+    class Meta:
+        db_table="student"
+
+class product(models.Model):
+    productName = models.CharField(max_length=70)
+    productPrice = models.IntegerField()
+    productStock = models.PositiveIntegerField()
+    productColor = models.CharField(max_length=50,null=True)
+    productStatus = models.BooleanField(default=True)
+    productDescription = models.TextField(null=True)
+    class Meta:
+        db_table="Product"
+
+class car(models.Model):
+    carName = models.CharField(max_length=60)
+    carModelYear = models.IntegerField(max_length=4)
+    carDescription = models.CharField(max_length=100)
+    carFeatures = models.TextField(null=True)
+    carBuyDate = models.DateField(null=True)
+    carOwner = models.CharField(null= True, max_length=50)
+    carPrice = models.IntegerField(null=True)
+
+    class Meta:
+        db_table="Car"
