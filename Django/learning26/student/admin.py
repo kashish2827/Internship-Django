1a405e953cbf10af36e6fd7eedb8529a3a82d1bd
Kashish Jansari <jansarikashish@gmail.com>
2026-02-06 22:23:05 +0530

    06-02-26:make a table using one-to-one relation and one-to-many relation using foreign key

--- Django/learning26/student/admin.py
+++ Django/learning26/student/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
-from .models import student,product,car
+from .models import student,product,car,gymuser,gymuserprofile,gym,city,Trainer,Member
  
 # Register your models here.
 
 admin.site.register(student)
 admin.site.register(product)
 admin.site.register(car)
+admin.site.register(gymuser)
+admin.site.register(gymuserprofile)
+admin.site.register(city)
+admin.site.register(gym)
+admin.site.register(Trainer)
+admin.site.register(Member)
--- Django/learning26/student/models.py
+++ Django/learning26/student/models.py
@@ -22,7 +22,7 @@ class product(models.Model):
 
 class car(models.Model):
     carName = models.CharField(max_length=60)
-    carModelYear = models.IntegerField(max_length=4)
+    carModelYear = models.IntegerField()
     carDescription = models.CharField(max_length=100)
     carFeatures = models.TextField(null=True)
     carBuyDate = models.DateField(null=True)
@@ -31,3 +31,80 @@ class car(models.Model):
 
     class Meta:
         db_table="Car"
+
+class gymuser(models.Model):
+    userName = models.CharField(max_length=50)
+    userEmail = models.EmailField(null=True)
+    userPassword = models.CharField(max_length=100)
+    userPhone = models.CharField(max_length=15, null=True)
+    userAddress = models.TextField(null=True)
+
+    class Meta:
+        db_table="GYMUser"
+    def __str__(self):
+        return self.userName
+
+class gymuserprofile(models.Model):
+    bodytype = (('Ectomorph', 'Ectomorph'),
+                ('Mesomorph', 'Mesomorph'),
+                ('Endomorph', 'Endomorph'))
+    time = (('Morning', 'Morning'),
+            ('Afternoon', 'Afternoon'),
+            ('Evening', 'Evening'))
+    userId = models.OneToOneField(gymuser, on_delete=models.CASCADE)
+    bodytype = models.CharField(max_length=50,choices=bodytype)
+    dateOfBirth = models.DateField(null=True)
+    workouttime = models.CharField(max_length=50,choices=time)
+
+    class Meta:
+        db_table="GYMUserProfile"
+    def __str__(self):
+        return self.userId.userName
+    
+class city(models.Model):
+    cityName=models.CharField(max_length=50)
+
+    class Meta:
+        db_table="GYM City"
+    
+    def __str__(self):
+        return self.cityName
+    
+class gym(models.Model):
+    gymName = models.CharField(max_length=100)
+    gymLocation = models.CharField(max_length=100)
+    gymContact = models.CharField(max_length=15)
+    gymEmail = models.EmailField(null=True)
+    gymDescription = models.TextField(null=True)
+    cityID=models.ForeignKey(city,on_delete=models.CASCADE)
+
+    class Meta:
+        db_table="GYM"
+
+    def __str__(self):
+        return self.gymName
+   
+class Trainer(models.Model):
+    TrainerName = models.CharField(max_length=50)
+    TrainerCity = models.CharField(max_length=50)
+    TrainerContact = models.CharField(max_length=10)
+    TrainerEmail = models.EmailField()
+    cityId = models.ForeignKey(city,on_delete=models.CASCADE)
+
+    class Meta:
+        db_table="Trainer"
+    def __str__(self):
+        return self.TrainerName
+    
+class Member(models.Model):
+    MemberName = models.CharField(max_length=50)
+    MemberCity = models.CharField(max_length=50)
+    MemberEmail = models.EmailField()
+    MemberContact = models.CharField(max_length=10)
+    MemberID = models.ForeignKey(Trainer,on_delete=models.CASCADE)
+    #cityId = models.ForeignKey(city,on_delete=models.CASCADE,null=True)
+    class Meta:
+        db_table="Member"
+    def __str__(self):
+            return self.MemberName
+        
