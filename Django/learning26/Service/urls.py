204891541f2e80dd4a1d6b7c2a40051aad95865f
Kashish Jansari <jansarikashish@gmail.com>
2026-02-16 22:35:22 +0530

    16-02-26:create a service app and perform CRUD operation on this table

--- Django/learning26/Service/urls.py
+++ Django/learning26/Service/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('service/',views.Servicelist,name="servicelist"),
-    path('serviceadd',views.AddService,name="addservice")
+    path('serviceadd',views.AddService,name="addservice"),
+    path('updateservice/<int:id>',views.updateservice,name='updateservice'),
+    path('deleteservice/<int:id>',views.deleteservice,name='deleteservice')
 ]
--- Django/learning26/Service/views.py
+++ Django/learning26/Service/views.py
@@ -5,7 +5,7 @@ from .models import Services
 # Create your views here.
 def AddService(request):
     print(request.method)
-    if request.method == "POST":
+    if request.method == "POST":    
         form = ServiceForm(request.POST)
         form.save()
         return redirect("servicelist")
@@ -17,3 +17,18 @@ def Servicelist(request):
     service = Services.objects.all().order_by('id').values()
     print(service)
     return render(request,'Service/servicelist.html',{'service':service})
+
+def updateservice(request,id):
+    service = Services.objects.get(id=id)
+    if request.method == "POST":
+        form = ServiceForm(request.POST,instance=service) # instance get the data from the database and fill in the form
+        form.save()
+        return redirect("servicelist")
+    else:
+        form = ServiceForm(instance=service)
+        return render(request,'Service/serviceadd.html',{'form':form})
+    
+def deleteservice(request,id):
+    print("id from url = ",id)
+    Services.objects.filter(id=id).delete()
+    return redirect("servicelist")
